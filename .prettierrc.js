b42520e7cc83c5377af4a52ad54dbd37f2b80068
Rostislav Kozar <r.kozar@andersenlab.com>
2020-05-07 22:54:00 +0300

    Initial project setup

---
+++ .prettierrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  printWidth: 100,
+  useTabs: false,
+  tabWidth: 2,
+  semi: true,
+  singleQuote: true,
+  proseWrap: 'always',
+  quoteProps: 'as-needed',
+  arrowParens: 'avoid',
+  trailingComma: 'es5',
+  bracketSpacing: false,
+  jsxBracketSameLine: false,
+  jsxSingleQuote: false,
+};
